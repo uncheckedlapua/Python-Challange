0d58fad49943434e169fc418779e4dba9f462d12
Steven Crutcher <georgie.prescott@gmail.com>
2021-03-13 21:46:05 -0600

    corrections

--- PyBank.py
+++ PyBank.py
@@ -2,12 +2,5 @@
 import os
 import csv
 #read in csv file
-os.path.join('Resources','budget_data.csv')
+budget_data=os.path.join('budget_data.csv')
 #create lists
-profits=[]
-monthly_change=[]
-date=[]
-count=0
-total_profit=0
-total_change_profits=0
-initial_profit=0
